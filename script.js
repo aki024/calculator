0dd04124322dd85e30f0ebd3a3b8996d875b02bb
aki024 <aleksa.simicc7@gmail.com>
2022-02-02 21:43:24 +0100

    Added most of the functionality only thing that is left is design and keyboard support

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 const display = document.querySelector('.result'); //Display the numbers
 const numbers = document.querySelectorAll('.number'); //Number buttons
-const addNumber = document.querySelector('#numberAdd');
-const subtractNumber = document.querySelector("#numberSubtract");
-const multiplyNumber = document.querySelector("#numberMultiply");
-const divideNumber = document.querySelector("#numberDivide");
-const result = document.querySelector('#numberResult')
-const allClear = document.querySelector('#numberAC');
-const backspace = document.querySelector("#numberBackspace");
-let operation = "";
-let saveOperation = "";
+const addNumber = document.querySelector('#numberAdd'); // plus button
+const subtractNumber = document.querySelector("#numberSubtract"); //minus button
+const multiplyNumber = document.querySelector("#numberMultiply"); //multiply button
+const divideNumber = document.querySelector("#numberDivide"); // divide button
+const result = document.querySelector('#numberResult') // equals button
+const allClear = document.querySelector('#numberAC'); // All clear button
+const backspace = document.querySelector("#numberBackspace"); //Delete one button
+const decimal = document.querySelector('#numberDot'); //Decimal button
+const displayCheck = document.querySelectorAll('button');
+
+let numberResult;
+
+let operation = ""; //Operation ()
+let saveOperation = ""; // variable to store operation once its cleared
 
 const operatorButtons = document.querySelectorAll('.operator') //operator buttons (+ - / *)
 
-let storeNumberA = null;
+let storeNumberA = null; // we need to store numbers so here are two variables for that
 let storeNumberB = null;
 
-let previousCalcOperation = null;
+let previousCalcOperation = null; //we also need to store variables so this is that
 let calcOperation = null;
 
-
+//On each operator click we get the button value (+ - / *)
 operatorButtons.forEach(button => {
     button.addEventListener('click', (e) => {
         operation = e.target.textContent;
@@ -31,44 +36,43 @@ numbers.forEach(number => {
 
 
         display.innerHTML += e.target.textContent;
-
+        //first click of a number 
         if (calcOperation === null && operation === "") {
-            calcOperation = "numberPress";
-            storeNumberA = display.innerHTML;
+            calcOperation = "numberPress"; //curent thing that is happening is numberPress
+            storeNumberA = parseFloat(display.innerHTML); // store input number from the display
 
-            console.log(previousCalcOperation, calcOperation, storeNumberA, storeNumberB, operation, 'prvi');
+
         } else if (calcOperation === "numberPress" && storeNumberB === null && operation === "") {
             calcOperation = "numberPress";
-            storeNumberA = display.innerHTML;
-            console.log('jossj');
-            console.log(previousCalcOperation, calcOperation, storeNumberA, storeNumberB, operation, 'dr');
-        } else if (previousCalcOperation === "numberPress" && calcOperation === "operation" && operation === "") {
-            previousCalcOperation = calcOperation;
-            calcOperation = "numberPress";
-            storeNumberA = display.innerHTML;
-            console.log(previousCalcOperation, calcOperation, storeNumberA, storeNumberB, operation, 'tr');
+            storeNumberA = parseFloat(display.innerHTML);
+
+            //second number click
+
+            //check if divided by zero
+        } else if (previousCalcOperation === "operation" && calcOperation === "numberPress" && operation === "" && display.innerHTML.includes('Lma')) {
+            display.innerHTML = "";
+            clearDisplay();
 
         } else if (previousCalcOperation === "operation" && calcOperation === "numberPress" && operation === "") {
-            storeNumberA = display.innerHTML;
+            storeNumberA = parseFloat(display.innerHTML); //storing purposes
 
-            console.log('opaa');
-            console.log(previousCalcOperation, calcOperation, storeNumberA, storeNumberB, operation, 'cr');
+            //check if operation has a value
         } else if (operation === "+" || operation === "-" || operation === "*" || operation === "/") {
-            saveOperation = operation;
-            operation = "";
-            display.innerHTML = "";
-            display.innerHTML += e.target.textContent;
-            storeNumberA = display.innerHTML;
-            previousCalcOperation = calcOperation;
-            calcOperation = "numberPress";
-            console.log(previousCalcOperation, calcOperation, storeNumberA, storeNumberB, operation, saveOperation, 'peti');
+            saveOperation = operation; //save the operation value
+            operation = ""; //clear so we can input more numbers
+            display.innerHTML = ""; // display is empty after we click a sign
+            display.innerHTML += e.target.textContent; //add numbers
+            storeNumberA = parseFloat(display.innerHTML); //store numbers
+            previousCalcOperation = calcOperation; //get value of previus operation and store it in a var
+            calcOperation = "numberPress"; //add the number press operation as current
+
         }
 
     })
 });
 
 //THE OPERATIONS
-// Storing first number clearing the display and setting the operation for result to value
+// Function that does all this is down
 addNumber.addEventListener('click', calculateWithoutEqual)
     //Subtraction
 subtractNumber.addEventListener('click', calculateWithoutEqual)
@@ -77,24 +81,33 @@ multiplyNumber.addEventListener('click', calculateWithoutEqual)
     //Division
 divideNumber.addEventListener('click', calculateWithoutEqual)
 
-
-
 //RESULT FUNCTIONALITY
 result.addEventListener('click', () => {
+    //because we clear the value of the button click so it works we need to use saveOperation which was stored in button click
+    if (saveOperation === "+" && storeNumberA !== null) {
+        numberResult = parseFloat(storeNumberB) + parseFloat(storeNumberA);
+        display.innerHTML = Math.round((numberResult) * 100) / 100;
+        //quick math round it to 2 decimals
+    } else if (saveOperation === "-" && storeNumberA !== null) {
+        numberResult = parseFloat(storeNumberB) - parseFloat(storeNumberA);
+        display.innerHTML = Math.round((numberResult) * 100) / 100;
+    } else if (saveOperation === "*" && storeNumberA !== null) {
+        numberResult = parseFloat(storeNumberB) * parseFloat(storeNumberA);
+        display.innerHTML = Math.round((numberResult) * 100) / 100;
+    } else if (saveOperation === "/" && storeNumberA !== null) {
+        if (storeNumberA === 0) {
+            display.innerHTML = "Lmao";
+            //divide by 0
+        } else {
+            numberResult = parseFloat(storeNumberB) / parseFloat(storeNumberA);
 
-    console.log('p');
-
-    if (saveOperation === "+") {
-        display.innerHTML = parseInt(storeNumberB) + parseInt(storeNumberA);
-
-    } else if (saveOperation === "-") {
-        display.innerHTML = parseInt(storeNumberB) - parseInt(storeNumberA);
-    } else if (saveOperation === "*") {
-        display.innerHTML = parseInt(storeNumberB) * parseInt(storeNumberA);
-    } else if (saveOperation === "/") {
-        display.innerHTML = parseInt(storeNumberB) / parseInt(storeNumberA);
+            display.innerHTML = Math.round((numberResult) * 100) / 100;
+        }
+    } else if (saveOperation === "" && storeNumberB === null && storeNumberA === null) {
+        display.innerHTML = "";
+        //equal sign before everything 
     } else {
-        display.innerHTML = display.innerHTML;
+
     }
 
 })
@@ -103,6 +116,7 @@ allClear.addEventListener('click', clearDisplay);
 //Delete one character
 backspace.addEventListener('click', () => {
     display.innerHTML = display.innerHTML.slice(0, -1);
+    storeNumberA = parseFloat(display.innerHTML);
 })
 
 //All clear
@@ -110,50 +124,72 @@ function clearDisplay() {
     display.innerHTML = "";
     storeNumberB = null;
     storeNumberA = null;
-
+    operation = "";
+    saveOperation = "";
+    calcOperation = null;
+    previousCalcOperation = null;
 }
+//Decimal support
+decimal.addEventListener('click', () => {
+    //limit to 1 decimal
+    if (display.innerHTML.includes('.')) {
+
+    } else {
+        display.textContent += '.';
+    }
+})
 
 function calculate() {
     if (saveOperation === "+") {
-        display.innerHTML = parseInt(storeNumberB) + parseInt(storeNumberA);
-
+        numberResult = parseFloat(storeNumberB) + parseFloat(storeNumberA);
+        //take the saved operator which i stored on number click and do the math
 
     } else if (saveOperation === "-") {
-        display.innerHTML = parseInt(storeNumberB) - parseInt(storeNumberA);
+        numberResult = parseFloat(storeNumberB) - parseFloat(storeNumberA);
 
+    } else if (saveOperation === "*") {
+        numberResult = parseFloat(storeNumberB) * parseFloat(storeNumberA);
 
-    } else if (operation === "*") {
-        display.innerHTML = parseInt(storeNumberB) * parseInt(storeNumberA);
-    } else if (operation === "/") {
-        display.innerHTML = parseInt(storeNumberB) / parseInt(storeNumberA);
+    } else if (saveOperation === "/") {
+        if (storeNumberA === 0) {
+            display.innerHTML = "Lmao"; //divide by 0 again
+        } else {
+            numberResult = parseFloat(storeNumberB) / parseFloat(storeNumberA);
+        }
+
     } else {
         display.innerHTML = display.innerHTML;
     }
-
+    display.innerHTML = Math.round((numberResult) * 100) / 100;
 }
-
-function calculateWithoutEqual() {
-    if (calcOperation === "numberPress" && previousCalcOperation === null) {
-        previousCalcOperation = calcOperation;
-        calcOperation = "operation";
-        storeNumberB = storeNumberA;
-        storeNumberA = null;
-        display.innerHTML = "";
-        console.log(previousCalcOperation, calcOperation, storeNumberA, storeNumberB, operation, 'fjedan');
+//This is where the magic happens
+function calculateWithoutEqual() { //First run trough
+    if (calcOperation === "numberPress" && previousCalcOperation === null && storeNumberA === null || storeNumberB === null) {
+        previousCalcOperation = calcOperation; //numberPress becomes previousCalculation
+        calcOperation = "operation"; // operation becomes the new calcOperation
+        storeNumberB = storeNumberA; // We store our number
+        storeNumberA = null; //we delete the old one and store another one up in the code
 
     } else if (previousCalcOperation === "operation" && calcOperation === "numberPress") {
+        //if our previus operation was (+-/*) and our new operation is numberPress
+        display.innerHTML = ""; //clear number
 
+        calculate(); //run calculate function
 
-
-        display.innerHTML = "";
-
-        calculate();
-
-        previousCalcOperation = calcOperation;
-        calcOperation = "operation";
-        storeNumberB = display.innerHTML;
+        previousCalcOperation = calcOperation; // previusCalculation becomes this value
+        calcOperation = "operation"; // 
+        storeNumberB = parseFloat(display.innerHTML);
         storeNumberA = null;
-        console.log(previousCalcOperation, calcOperation, storeNumberA, storeNumberB, operation, 'fdva');
 
     }
 }
+
+displayCheck.forEach(button => {
+    button.addEventListener('click', () => {
+        if (display.innerHTML.length > 14) { //limit to 14 digits
+            display.innerHTML = display.innerHTML.substring(0, 14);
+        }
+    })
+
+
+})
